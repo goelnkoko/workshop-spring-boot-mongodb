c8535120aee63796f166087bb509edd7c2f1fbb6
Goel Nkoko <goelnkoko12@hotmail.com>
2023-11-06 01:14:10 +0100

    User Insertion with POST

--- src/main/java/com/nkumbo/workshopmongo/services/UserService.java
+++ src/main/java/com/nkumbo/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.nkumbo.workshopmongo.services;
 
 import com.nkumbo.workshopmongo.domain.User;
+import com.nkumbo.workshopmongo.dto.UserDTO;
 import com.nkumbo.workshopmongo.repository.UserRepository;
 import com.nkumbo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +26,13 @@ public class UserService {
 
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Not found!"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
