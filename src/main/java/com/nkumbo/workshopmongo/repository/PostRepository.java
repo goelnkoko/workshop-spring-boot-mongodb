cffaf27bcd828728a8e5d49d2796e0814700b346
Goel Nkoko <goelnkoko12@hotmail.com>
2023-11-07 00:30:32 +0100

    Simple query with query methods

--- src/main/java/com/nkumbo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nkumbo/workshopmongo/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.nkumbo.workshopmongo.repository;
 import com.nkumbo.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
