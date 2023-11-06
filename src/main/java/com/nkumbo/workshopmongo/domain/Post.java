2c418e7b3b2896b26241e07d1526ccd7d48b68f5
Goel Nkoko <goelnkoko12@hotmail.com>
2023-11-06 21:46:04 +0100

    Adding comments to posts

--- src/main/java/com/nkumbo/workshopmongo/domain/Post.java
+++ src/main/java/com/nkumbo/workshopmongo/domain/Post.java
@@ -1,10 +1,13 @@
 package com.nkumbo.workshopmongo.domain;
 
 import com.nkumbo.workshopmongo.dto.AuthorDTO;
+import com.nkumbo.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,6 +20,8 @@ public class Post {
     private String body;
 
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){
 
@@ -68,6 +73,14 @@ public class Post {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
