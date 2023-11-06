c7c18e4f667001d2a63e420688542733aa113c1a
Goel Nkoko <goelnkoko12@hotmail.com>
2023-11-06 01:01:43 +0100

    Getting a user by i

--- src/main/java/com/nkumbo/workshopmongo/services/UserService.java
+++ src/main/java/com/nkumbo/workshopmongo/services/UserService.java
@@ -2,10 +2,14 @@ package com.nkumbo.workshopmongo.services;
 
 import com.nkumbo.workshopmongo.domain.User;
 import com.nkumbo.workshopmongo.repository.UserRepository;
+import com.nkumbo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +19,11 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id){
+
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Not found!"));
     }
 }
